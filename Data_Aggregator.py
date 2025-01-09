fcefecf0d6b03b2b9450c9ea59acaf4bd991368b
JayasuryaThirumaran <jayasurya.t@talentsprint.com>
2025-01-09 08:15:40 +0000

    MADE CHANGES

---
+++ Data_Aggregator.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Assume dataset1 and dataset2 are already loaded as pandas DataFrames
+# dataset1 is the main data, and dataset2 is the data you want to add
+
+dataset1 = pd.read_csv("data/Data_Main.csv")  # Example loading dataset1
+dataset2 = pd.read_csv("Month2.csv")  # Example loading dataset2
+
+# Append rows of dataset2 to dataset1
+dataset1 = pd.concat([dataset1, dataset2], ignore_index=True)
+
+dataset1.to_csv("data/Data_Main.csv", index=False) 
