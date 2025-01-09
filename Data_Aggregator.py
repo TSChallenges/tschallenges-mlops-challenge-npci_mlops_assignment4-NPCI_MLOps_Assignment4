76330449e7fd325039f73e18bf214fc6dde6c28b
JayasuryaThirumaran <jayasurya.t@talentsprint.com>
2025-01-09 08:19:44 +0000

    made changes

--- Data_Aggregator.py
+++ Data_Aggregator.py
@@ -7,6 +7,6 @@ dataset1 = pd.read_csv("data/Data_Main.csv")  # Example loading dataset1
 dataset2 = pd.read_csv("Month2.csv")  # Example loading dataset2
 
 # Append rows of dataset2 to dataset1
-dataset1 = pd.concat([dataset1, dataset2], ignore_index=True)
+dataset1 = pd.concat([dataset1, dataset2],axis=0, ignore_index=True)
 
 dataset1.to_csv("data/Data_Main.csv", index=False) 
